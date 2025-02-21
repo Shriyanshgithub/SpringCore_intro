7a869e54e69e6fdc5eb737a1e838a986094d4d17
Shriyansh Gupta <gshriyansh703@gmail.com>
2025-02-21 21:18:09 +0530

    SpringCore intro

---
+++ springcoreIntro/src/main/java/com/capgemini/springcoreIntro/SpringcoreIntroApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.springcoreIntro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreIntroApplication.class, args);
+	}
+
+}
